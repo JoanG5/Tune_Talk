0430fb27e3e08cff6e61dafc66d24d6f591031dd
Paulo Xu <pauloxu@PauloAir.local>
2024-04-15 18:04:52 -0400

    added slider for song preview, and UI changes

--- client/src/components/SongDisplay.jsx
+++ client/src/components/SongDisplay.jsx
@@ -4,8 +4,9 @@ import Paper from '@mui/material/Paper';
 import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
-import { grey } from '@mui/material/colors';
-
+import MusicPlayerSlider from './SongPreview';
+import Divider from "@mui/material/Divider";
+import Rating from '@mui/material/Rating';
 
 function SongDisplay({props}) {
   const duration = (ms) => {
@@ -33,68 +34,53 @@ function SongDisplay({props}) {
             flexGrow: 1,
           }}
           >
-          <Grid container spacing={3} direction='row' alignItems='center' paddingRight={3}>
+          <Grid container spacing={3} direction='row' justifyContent='center' alignItems='center' paddingRight={3}>
             <Grid item xs={3} >
                 <Img alt={props.album.name} src={props.album.images[0].url} />
             </Grid>
-            <Grid item xs={4}>
+            <Grid item xs={3}>
                 <Typography fontWeight={700} gutterBottom variant="subtitle2" component="span">
                   Song
                 </Typography>
-                <Typography fontWeight={900} variant="h5">
+                <Typography fontWeight={800} variant="h5">
                   {props.name}
                 </Typography>
                 <Typography fontWeight={400} gutterBottom variant="subtitle2" component="span">
                   {props.artists[0].name} • {props.album.name} • {duration(props.duration_ms)} 
                 </Typography>
+                <Typography fontWeight={400} gutterBottom variant="subtitle2" mt={2}>
+                  Song preview
+                </Typography>
+                <Box sx={{mr:'64px', minWidth: '128px'}}>
+                  <MusicPlayerSlider src={props.preview_url} />
+                </Box>
               </Grid>
-              <Grid item xs={2}>
-                <Box sx={{ bgcolor: grey[800], color: grey[200], p: '12px' }}>
-                  <Typography fontWeight={700} gutterBottom variant="subtitle1" >
+              <Grid item xs={3}>
+                <Box sx={{ border: '2px solid grey', p: '12px', minWidth: '200px', minHeight: '185px'}}>
+                  <Typography fontWeight={700} gutterBottom component={'legend'} >
                     User Score
                   </Typography>
-                  <Typography fontWeight={900} gutterBottom variant="h4" >
-                    80
-                  </Typography>
+                  <Rating name="read-only" value={4} readOnly /> <br />
                   <Typography gutterBottom variant="caption">
                     Based on 100 ratings
                   </Typography>
                 </Box>
             </Grid>
-            <Grid item xs={3}>
-                <Box sx={{ bgcolor: grey[800], color: grey[200], p: '12px' }}>
-                  <Typography fontWeight={700} gutterBottom variant="subtitle1" >
-                    Release Date:
+            <Grid item xs>
+                <Box sx={{ border: '2px solid grey', p: '12px', minWidth: '250px' }}>
+                  <Typography fontWeight={700} gutterBottom variant="subtitle2" >
+                    Details
                   </Typography>
-                  <Typography fontWeight={900} gutterBottom variant="h4" >
-                  {props.album.release_date}
-                  </Typography>
-                  <Typography fontWeight={700} gutterBottom variant="subtitle1" >
+                  <Divider />
+                  <Typography fontWeight={400} gutterBottom variant="subtitle2" mt={'16px'} >
+                  Release Date: {props.album.release_date} <br />
                   Album: {props.album.name} <br />
                   Type: {props.type}<br />
                   Track number: {props.track_number}<br />
                   Genre: {props.artists.genres}
-                
                   </Typography>
                 </Box>
             </Grid>
-            <Grid item xs={12}>
-                <Box sx={{ bgcolor: grey[800], color: grey[200], p: '10px' }}>
-                  
-                  
-                <iframe
-                  
-                  title="Spotify Embed"
-                  src={props.preview_url}
-                  width="100%"
-                  height="100%"
-                  
-                  allow="encrypted-media"
-                ></iframe>
-                
-                </Box>
-            </Grid>
-
           </Grid>
         </Paper>
       )}
