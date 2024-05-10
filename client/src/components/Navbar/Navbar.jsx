78f13e6f5e5d3ac0d2d11f6f6cecc698940b1db0
angvit <angelo.vitalino@baruchmail.cuny.edu>
2024-05-09 23:13:55 -0400

    boilerplate navbar

---
+++ client/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { AppBar, Toolbar, Box } from "@mui/material";
+
+export const Navbar = () => {
+  return (
+    <Box height={"100vh"}>
+      <AppBar>
+        <Toolbar></Toolbar>
+      </AppBar>
+    </Box>
+  );
+};
