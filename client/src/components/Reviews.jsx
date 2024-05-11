b54287d5c3b07f7e0186a61b421d29237528e125
popki222 <135931155+popki222@users.noreply.github.com>
2024-05-10 22:05:59 -0400

    added review feature to songpage

--- client/src/components/Reviews.jsx
+++ client/src/components/Reviews.jsx
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React, { useState } from 'react';
 import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
 import Divider from '@mui/material/Divider';
@@ -8,53 +8,63 @@ import Typography from '@mui/material/Typography';
 import Paper from '@mui/material/Paper';
 import Rating from '@mui/material/Rating';
 import Grid from '@mui/material/Grid';
+import PostReview from './PostReview';
 
-function ReviewList({reviews}){
+function Reviews() {
+    const [reviews, setReviews] = useState([]);
 
-    return(
-        <div >
+    
+    const addReview = (review) => {
+        setReviews([...reviews, review]);
+    };
+
+    return (
+        <div>
             <Grid container spacing={0} direction='row'>
-            <Grid item xs={2} ></Grid>
-            <Grid item xs={5} >
-             <Paper elevation={0}
-                sx={{
-                    p: 2,
-                    margin: "auto",
-                    minWidth: '400px',
-                    flexGrow: 1,
-                    maxWidth: '100%'
-                }}
-                >
-                <Typography fontWeight={700} gutterBottom variant="subtitle1" component="span">
-                  User Reviews
-                </Typography>
-                <Divider />
+                <Grid item xs={2}></Grid>
+                <Grid item xs={5}>
+                    <Paper elevation={0}
+                        sx={{
+                            p: 2,
+                            margin: "auto",
+                            minWidth: '400px',
+                            flexGrow: 1,
+                            maxWidth: '100%'
+                        }}
+                    >
+                        <Typography fontWeight={700} gutterBottom variant="subtitle1" component="span">
+                            User Reviews
+                        </Typography>
+                        <Divider />
 
-                {reviews.length === 0 ? (
-                <p>No reviews yet.</p>
-                ) : (
-                <List sx={{ width: '100%', bgcolor: 'background.paper' }}>
-                    <ListItem alignItems="flex-start">
-                        <ListItemAvatar>
-                        <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
-                        </ListItemAvatar>
-                        <div class="grid grid-flow-row-dense">
-                            <div class="col-span-2">User <br />
-                            <Rating name="read-only" value={4} readOnly size='small' /> <br />
-                            </div>
-                            <div class="col-span-2">comment</div>
-                        </div>
-                    
-                    </ListItem>
-                    <Divider/>
-                </List>
-        )}
-        </Paper>
-        </Grid> 
-        </Grid>
+                        {reviews.length === 0 ? (
+                            <p>No reviews yet.</p>
+                        ) : (
+                            <List sx={{ width: '100%', bgcolor: 'background.paper' }}>
+                                {reviews.map((review, index) => (
+                                    <div key={index}>
+                                        <ListItem alignItems="flex-start">
+                                            <ListItemAvatar>
+                                                <Avatar alt="User Avatar" src="/static/images/avatar/1.jpg" />
+                                            </ListItemAvatar>
+                                            <div className="grid grid-flow-row-dense">
+                                                <div className="col-span-2">User <br />
+                                                    <Rating name="read-only" value={review.rating} readOnly size='small' /> <br />
+                                                </div>
+                                                <div className="col-span-2">{review.comment}</div>
+                                            </div>
+                                        </ListItem>
+                                        <Divider />
+                                    </div>
+                                ))}
+                            </List>
+                        )}
+                    </Paper>
+                </Grid>
+            </Grid>
+            <PostReview addReview={addReview} />
         </div>
     );
-};
+}
 
-
-export default ReviewList;
+export default Reviews;
