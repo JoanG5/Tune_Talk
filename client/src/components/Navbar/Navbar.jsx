7a7192e44711135404b6df806d58389f76ba541c
angvit <angelo.vitalino@baruchmail.cuny.edu>
2024-05-11 13:59:25 -0400

    Auth0 logic & packages

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,11 +1,17 @@
 import React from "react";
 import { AppBar, Toolbar, Box } from "@mui/material";
+import LoginButton from "../LoginButton/LoginButton";
+import LogoutButton from "../LogoutButton/LogoutButton";
+import { useAuth0 } from "@auth0/auth0-react";
 
 export const Navbar = () => {
+  const { user, isAuthenticated } = useAuth0();
   return (
     <Box height={"100vh"}>
       <AppBar>
-        <Toolbar></Toolbar>
+        <Toolbar>
+          {isAuthenticated ? <LogoutButton /> : <LoginButton />}
+        </Toolbar>
       </AppBar>
     </Box>
   );
