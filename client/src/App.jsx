a5531474cb66f4cd4bcd31f9ed97ae7052d14ebe
Joan <Joanguzman553@gmail.com>
2024-05-15 13:00:25 -0400

    Fixed Auth0 issue by wrapping the mainjsx with the auth0provider, aswell as made the navbar included with all the pages and creating links to each page, still an issue with profile

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,30 +6,30 @@ import Song from "./pages/Song";
 import Profile from "./pages/Profile";
 import SavedSongs from "./pages/SavedSongs";
 import SavedAlbums from "./pages/SavedAlbums";
-import Auth0ProviderWithHistory from "./auth0Provider";
+import Navbar from "./components/Navbar/Navbar";
+import Loading from "./components/Loading";
 import { useAuth0 } from "@auth0/auth0-react";
 
 function App() {
   const { isLoading, error } = useAuth0();
 
   if (isLoading) {
-    return <div>Loading ...</div>;
+    return <Loading />;
   }
 
   return (
-    <Auth0ProviderWithHistory>
-      <Router>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/album" element={<Album />} />
-          <Route path="/song" element={<Song />} />
-          <Route path="/profile" element={<Profile />} />
-          <Route path="/profile/savedsong" element={<SavedSongs />} />
-          <Route path="/profile/savedalbum" element={<SavedAlbums />} />
-          <Route path="/test" element={<Test />} />
-        </Routes>
-      </Router>
-    </Auth0ProviderWithHistory>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/album" element={<Album />} />
+        <Route path="/song" element={<Song />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/profile/savedsong" element={<SavedSongs />} />
+        <Route path="/profile/savedalbum" element={<SavedAlbums />} />
+        <Route path="/test" element={<Test />} />
+      </Routes>
+    </Router>
   );
 }
 
--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,11 +1,9 @@
 import { React, useState } from "react";
 import { AppBar, Toolbar, Box, Typography, Tab, Tabs } from "@mui/material";
-import { Link, useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from "react-router-dom";
 import LoginButton from "../LoginButton/LoginButton";
 import LogoutButton from "../LogoutButton/LogoutButton";
 import { useAuth0 } from "@auth0/auth0-react";
-import { logoImg } from "../../assets";
-
 
 export const Navbar = () => {
   const { user, isAuthenticated } = useAuth0();
@@ -16,32 +14,48 @@ export const Navbar = () => {
   const handleChange = (event, newValue) => {
     setValue(newValue);
     switch (newValue) {
-        case 0:
-            navigate('/songs');
-            break;
-        case 1:
-            navigate('/albums');
-            break;
-        case 2:
-            navigate('/profile');
-            break;
-        default:
-            break;
+      case 0:
+        navigate("/songs");
+        break;
+      case 1:
+        navigate("/albums");
+        break;
+      case 2:
+        navigate("/profile");
+        break;
+      default:
+        break;
     }
   };
 
   return (
-    <Box height={"100vh"}>
-      <AppBar position="fixed" sx={{background: 'black'}}>
+    <Box classname="top-0">
+      <AppBar position="" sx={{ background: "black" }}>
         <Toolbar>
           <Box flexGrow={1}>
-            <Box display={'flex'} alignItems={'center'} gap={0.5}>
-              <img width={"54px"} height={"54px"} src={logoImg} alt="logo"></img>
-              <Typography variant="h5" sx={{width: 'fit-content'}}>
+            <Box display={"flex"} alignItems={"center"} gap={0.5}>
+              <img width={"54px"} height={"54px"} src={""} alt="logo"></img>
+              <Typography variant="h5" sx={{ width: "fit-content" }}>
                 TuneTalk
               </Typography>
-              <Tabs value={value} onChange={handleChange} aria-label="navigation tabs">
+              <Tabs
+                value={value}
+                onChange={handleChange}
+                aria-label="navigation tabs"
+              >
                 <Tab label="Home" />
+                <Link to="/Home">
+                  <Tab label="Home" />
+                </Link>
+                <Link to="/profile">
+                  <Tab label="Profile" />
+                </Link>
+                <Link to="/profile/savedsong">
+                  <Tab label="Saved Songs" />
+                </Link>
+                <Link to="/profile/savedalbum">
+                  <Tab label="Saved Albums" />
+                </Link>
                 <Tab label="Songs" />
                 <Tab label="Profile" />
               </Tabs>
@@ -53,3 +67,5 @@ export const Navbar = () => {
     </Box>
   );
 };
+
+export default Navbar;
