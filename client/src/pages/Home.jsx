1ec076e629c0b5b2199fca1a2bb714f7dbc600ff
Joan <joanguzman553@gmail.com>
2024-05-15 22:11:47 -0400

    added error and testing in home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,6 +1,11 @@
 import React from "react";
+import { useAuth0 } from "@auth0/auth0-react";
 
 function Home() {
+
+  const { user, isAuthenticated } = useAuth0();
+  console.log(isAuthenticated)
+
   return (
     <>
       <div className="text-3xl font-bold underline">Home</div>
