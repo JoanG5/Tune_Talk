38348d11706e06edd88251689d61946ca7070374
Paulo Xu <pauloxu@PauloAir.local>
2024-05-17 01:13:49 -0400

    edit and delete buttons for reviews

--- client/src/components/Reviews.jsx
+++ client/src/components/Reviews.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
 import Divider from '@mui/material/Divider';
@@ -8,17 +8,132 @@ import Typography from '@mui/material/Typography';
 import Paper from '@mui/material/Paper';
 import Rating from '@mui/material/Rating';
 import Grid from '@mui/material/Grid';
-import PostReview from './PostReview';
 import Box from '@mui/material/Box';
+import { Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, TextField, Button } from '@mui/material';
+import { getOneAlbumId } from "../services/Spotify";
+import { useParams } from "react-router-dom";
+import axios from "axios";
+import { useAuth0 } from "@auth0/auth0-react";
 
 
 function ReviewList() {
-    const [reviews, setReviews] = useState([]);
+    const [selectedReviewIndex, setSelectedReviewIndex] = useState(null);
+    const [editOpen, setEditOpen] = useState(false);
+    const [editedRating, setEditedRating] = useState(0);
+    const [editedReview, setEditedReview] = useState('');
+    const [open, setOpen] = useState(false);
+    const [rating, setRating] = useState(0);
 
-    const addReview = (review) => {
-        setReviews([...reviews, review]);
+    const { user, isAuthenticated } = useAuth0();
+    const [reviews, setReviews] = useState([]);
+    const [review, setReview] = useState("");
+
+    const albumTitle = "Nectar";
+    const { albumId } = useParams();
+
+
+    useEffect(() => {
+        const fetchAlbumInfo = async () => {
+          try {
+            const trackData = await getOneAlbumId(albumId);
+            const reviews = await axios.get(
+              `http://localhost:3000/albumReview/${albumId}`
+            );
+            setReviews(reviews.data);
+          } catch (error) {
+            console.error("Error fetching album info:", error);
+          }
+        };
+    
+        fetchAlbumInfo();
+      }, [albumTitle]);
+
+
+
+    const handleClickOpen = () => {
+        setOpen(true);
+    };
+
+    const handleClose = () => {
+        setOpen(false);
+    };
+
+
+    const handleSaveReview = async () => {
+        try {
+          const reviewData = {
+            review: review,
+            rating: rating,
+            user_id: user.sub,
+            spotify_id: albumId,
+          };
+    
+          const response = await axios.post(
+            "http://localhost:3000/albumReview",
+            reviewData
+          );
+          console.log(response);
+          setReviews([...reviews, reviewData]);
+          setOpen(false);
+        } catch (error) {
+          console.error("Error saving review:", error);
+        }
+      };
+
+      const handleEditClick = (reviewId, index) => {
+        setSelectedReviewIndex(index);
+        setEditedRating(reviews[index].rating);
+        setEditedReview(reviews[index].review);
+        setEditOpen(true);
     };
     
+
+
+    const handleEditClose = () => {
+        setSelectedReviewIndex(null);
+        setEditOpen(false);
+    };
+
+    const handleUpdateReview = async (review_id, index) => {
+        try {
+          const reviewData = {
+            review: review,
+            rating: rating,
+          };
+          const response = await axios.put(
+            `http://localhost:3000/albumReview/${user.sub}/${review_id}`,
+            reviewData
+          );
+          console.log(response);
+          setReviews((prevReviews) => {
+            const updatedReviews = [...prevReviews];
+            updatedReviews[index] = {
+              ...updatedReviews[index],
+              review: editedReview,
+              rating: editedRating,
+            };
+            handleEditClose();
+            return updatedReviews;
+          });
+        } catch (error) {
+          console.error("Error updating review:", error);
+        }
+      };
+
+    const handleDeleteReview = async (review_id, index) => {
+        try {
+          const response = await axios.delete(
+            `http://localhost:3000/albumReview/${user.sub}/${review_id}`
+          );
+          console.log(response);
+          setReviews((prevReviews) => prevReviews.filter((_, i) => i !== index));
+        } catch (error) {
+          console.error("Error deleting review:", error);
+        }
+      };
+
+
+
     return (
         <div style={{paddingLeft:'24px'}}>
             <Grid container spacing={0} direction='row'>
@@ -57,7 +172,9 @@ function ReviewList() {
                                                 <div className="col-span-2">User <br />
                                                     <Rating name="read-only" value={review.rating} readOnly size='small' /> <br />
                                                 </div>
-                                                <div className="col-span-2">{review.comment}</div>
+                                                <div className="col-span-2">{review.review}</div>
+                                                <Button onClick={() => handleEditClick(review.review_id, index)}>Edit</Button>
+                                                <Button onClick={() => handleDeleteReview(review.review_id, index)}>Delete</Button>
                                             </div>
                                         </ListItem>
                                         <Divider />
@@ -70,7 +187,85 @@ function ReviewList() {
             </Grid>
         </Grid>
         <br />
-        <PostReview addReview={addReview} />
+        <div>
+            <div style={{textAlign:"right", marginRight:'16px'}}>
+            <Button variant="outlined" color="primary" onClick={handleClickOpen}>
+                Add Review
+            </Button>
+            </div>
+
+            <Dialog open={open} onClose={handleClose}>
+                <DialogTitle>Add Review</DialogTitle>
+                <DialogContent>
+                    <DialogContentText>
+                        Please rate your experience and leave a comment.
+                    </DialogContentText>
+                    <Rating
+                        name="rating"
+                        value={rating}
+                        onChange={(event, newValue) => {
+                            setRating(newValue);
+                        }}
+                    />
+                    <TextField
+                        autoFocus
+                        margin="dense"
+                        id="review-comment"
+                        label="Your Review"
+                        multiline
+                        rows={6}
+                        type="text"
+                        fullWidth
+                        value={review}
+                        onChange={(event) => setReview(event.target.value)}
+                    />
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={handleClose} color="primary">
+                        Cancel
+                    </Button>
+                    <Button onClick={handleSaveReview} color="primary">
+                        Submit
+                    </Button>
+                </DialogActions>
+            </Dialog>
+        </div>
+
+        <Dialog open={editOpen} onClose={handleEditClose}>
+                <DialogTitle>Edit Review</DialogTitle>
+                <DialogContent>
+                    <DialogContentText>
+                        Please update your review.
+                    </DialogContentText>
+                    <Rating
+                        name="rating"
+                        value={rating}
+                        onChange={(event, newValue) => {
+                            setEditedRating(newValue);
+                        }}
+                    />
+                    <TextField
+                        autoFocus
+                        margin="dense"
+                        id="review-comment"
+                        label="Your Review"
+                        multiline
+                        rows={6}
+                        type="text"
+                        fullWidth
+                        value={review}
+                        onChange={(event) => setEditedReview(event.target.value)}
+                    />
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={handleEditClose} color="primary">
+                        Cancel
+                    </Button>
+                    <Button onClick={handleUpdateReview} color="primary">
+                        Submit
+                    </Button>
+                </DialogActions>
+            </Dialog>
         </div>
     );
 };
