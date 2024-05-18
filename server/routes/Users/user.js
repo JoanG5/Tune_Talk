ada3c9c7e2ec1a6a254685204e2ab0d232d7b21b
Joan <joanguzman553@gmail.com>
2024-05-18 00:49:32 -0400

    added user call to get a user name through their ID

--- server/routes/Users/user.js
+++ server/routes/Users/user.js
@@ -29,4 +29,13 @@ router
       });
   });
 
+router.route("/:id").get(async (req, res) => {
+  const user = await User.findOne({ where: { user_id: req.params.id } });
+  if (!user) {
+    res.status(404).send("User not found");
+    return;
+  }
+  res.status(200).json(user);
+});
+
 module.exports = router;
